4dc6fba90f0f997896c4bcadc39fe04640626ccd
adbchtr <adibbachtiar44@gmail.com>
2021-12-01 23:14:58 +0700

    change connection newest

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 
 func main() {
 	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "root:@tcp(194.163.40.147:3306)/campaign_api?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(xdbchtr.com)/campaign_api?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
